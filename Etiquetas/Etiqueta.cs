0064746c90f2d3399aa62a8fa8a5e3209fa4a7de
LeoNardoz333 <leo.vel333@gmail.com>
2024-02-21 12:06:42 -0600

    Se creó el acceso a datos de Etiquetas y Productos

--- Etiquetas/Etiqueta.cs
+++ Etiquetas/Etiqueta.cs
@@ -10,32 +10,31 @@ namespace Etiquetas
 {
     public class Etiqueta:IFunciones
     {
-        Base b = new Base("localhost","root","","");
-        public int Folio { get; set; }
-        public string Fecha { get; set; }
-        public string Producto { get; set; }
+        Base b = new Base("localhost", "userEtiquetador", "chivas123", "Etiquetas");
+        public int fkProducto { get; set; }
         public int Piezas { get; set; }
         public float Ancho { get; set; }
         public float Altura { get; set; }
         public void Borrar(int id)
         {
-            b.comando("delete from where id = "+id);
+            b.comando("delete from Etiqueta where id = "+id);
         }
 
         public void Crear(dynamic entidad)
         {
-            b.comando(string.Format("insert into values({0},'{1}','{2}',{3})",Folio,Fecha,Producto,Piezas));
+            b.comando(string.Format("insert into Etiqueta values({0},{1},{2},{3})",fkProducto,Piezas,Ancho,Altura));
         }
 
         public void Moficicar(int id)
         {
-            b.comando(string.Format("update from set folio={0}, fecha='{1}', producto='{2}', piezas={3}" +
-                " where id={4}", Folio, Fecha, Producto, Piezas,id));
+            b.comando(string.Format("update Etiqueta set fkProducto={0}, piezas={1}, ancho={2}, altura={3}"
+                +"where id={4}"
+                , fkProducto, Piezas, Ancho, Altura, id));
         }
 
         public DataSet Mostrar(string filtro)
         {
-            return b.Obtener(string.Format("Select * from where producto like '%{0}%'",filtro),"");
+            return b.Obtener(string.Format("Select * from Etiqueta"),"Etiqueta");
         }
     }
 }
---
+++ Etiquetas/Productos.cs
@@ -0,0 +1,39 @@
+﻿using ConectarRedReal;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etiquetas
+{
+    public class Productos : IFunciones
+    {
+        Base b = new Base("localhost", "userEtiquetador", "chivas123", "Etiquetas"); 
+        public int Folio { get; set; }
+        public string Fecha { get; set; }
+        public string Producto { get; set; }
+        public void Borrar(int id)
+        {
+            b.comando("delete from Datos where id = " + id);
+        }
+
+        public void Crear(dynamic entidad)
+        {
+            b.comando(string.Format("insert into Datos values({0},'{1}','{2}')", Folio, Fecha, Producto));
+        }
+
+        public void Moficicar(int id)
+        {
+            b.comando(string.Format("update Datos set folio={0}, fecha='{1}', producto='{2}'"
+                + "where id={3}"
+                , Folio, Fecha, Producto, id));
+        }
+
+        public DataSet Mostrar(string filtro)
+        {
+            return b.Obtener(string.Format("Select * from Datos where producto like '%{0}%'", filtro), "Datos");
+        }
+    }
+}
